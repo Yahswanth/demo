b6c9b2a5ff2475ee723ddd6532028ce527c88fe4
Rokkala Yahswanth <140070698+Yahswanth@users.noreply.github.com>
2024-12-21 12:06:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const request = require('request');
+const wikip = require('wiki-infobox-parser');
 
-app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+//ejs
+app.set("view engine", 'ejs');
+
+//routes
+app.get('/', (req,res) =>{
+    res.render('index');
 });
 
-app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+app.get('/index', (req,response) =>{
+    let url = "https://en.wikipedia.org/w/api.php"
+    let params = {
+        action: "opensearch",
+        search: req.query.person,
+        limit: "1",
+        namespace: "0",
+        format: "json"
+    }
+
+    url = url + "?"
+    Object.keys(params).forEach( (key) => {
+        url += '&' + key + '=' + params[key]; 
+    });
+
+    //get wikip search string
+    request(url,(err,res, body) =>{
+        if(err) {
+            response.redirect('404');
+        }
+            result = JSON.parse(body);
+            x = result[3][0];
+            x = x.substring(30, x.length); 
+            //get wikip json
+            wikip(x , (err, final) => {
+                if (err){
+                    response.redirect('404');
+                }
+                else{
+                    const answers = final;
+                    response.send(answers);
+                }
+            });
+    });
+
+    
 });
-app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
-});
-app.listen(process.env.PORT || 3000);
-module.exports = app;
+
+//port
+app.listen(3000, console.log("Listening at port 3000..."))
